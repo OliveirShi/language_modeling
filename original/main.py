bb5b6248c75bd2699ccd8f4e55901b4b87411960
Nan Jiang <jiangnan.rd@netease.com>
2016-09-30 16:53:38 +0800

    using tiny file for testing

--- original/main.py
+++ original/main.py
@@ -6,36 +6,38 @@ import numpy as np
 import theano
 import theano.tensor as T
 
-from rnn import *
+from rnnlm import *
 from utils import *
 
 lr=0.01
 p=0.5
 n_batch=10
+NEPOCH=100
 
 n_input=300
 n_hidden=500
 n_output=80000
 cell='gru'
-optimzer='sgd'
+optimizer='sgd'
+
 
 
 model=RNNLM(n_input,n_hidden,n_output,cell,optimizer,p)
 print 'training...'
 start=time.time()
 g_error=999.99
-for i in xrange(n_epoch):
+for epoch in xrange(NEPOCH):
     error=0
     in_start=time.time()
     for batch_id,xy in data_xy.items():
         cost=model.train(x,maskx,y,masky,lr)
         error+=cost
-        print i,
+        print epoch,
 
     error/=len(seqs)
     if error<g_error:
         g_error=error
-        save_model('./model.rnnlm.model_'+stri(i),model)
+        save_model('./model.rnnlm.model_'+str(i),model)
     print "Iter = "+str(i)+ ", Loss = "+str(error)+", Time = "+str(in_time)
     if error<e:
         break;
@@ -43,3 +45,17 @@ print "Finished. Time = "+str(time.time()-start)
 
 print "save model..."
 save_model("./model/rnnlm.model",model)
+
+
+def train_with_sgd(model, X_train, y_train, k, q_dis, q_w, learning_rate=0.001, nepoch=20, decay=0.9,
+    callback_every=10000, callback=None):
+    num_examples_seen = 0
+    for epoch in range(nepoch):
+        # For each training example...
+        print epoch
+        for i in np.random.permutation(len(y_train)):
+            # One SGD step
+            cost=model.sgd_step(X_train[i], y_train[i], negative_sample(y_train[i],k,q_dis), q_w, learning_rate, decay)
+            print cost,
+            num_examples_seen += 1
+    return model
--- original/utils.py
+++ original/utils.py
@@ -2,6 +2,16 @@ import numpy as np
 import theano
 import theano.tensor as T
 import cPickle as pickle
+import os
+import operator
+from os.path import isfile,join
+import csv
+import itertools
+import nltk
+
+SENTENCE_START_TOKEN='<S>'
+SENTENCE_END_TOKEN='</S>'
+UNKNOWN_TOKEN='<unk>'
 
 def save_model(f,model):
     ps={}
@@ -14,3 +24,67 @@ def load_model(f,model):
     for p in model.params:
         p.set_value(ps[p.name])
     return model
+
+
+def load_data(filename="data/reddit-comments-2015-08.csv", vocabulary_size=2000, min_sent_characters=0):
+    if isfile('data/dataset.pkl'):
+        with open('data/dataset.pkl')as f:
+            (X_train, y_train, word_to_index, index_to_word, sorted_vocab)=pickle.load(f)
+            return X_train, y_train, word_to_index, index_to_word, sorted_vocab
+
+    word_to_index = []
+    index_to_word = []
+
+    # Read the data and append SENTENCE_START and SENTENCE_END tokens
+    print("Reading CSV file...")
+
+    with open(filename, 'rt') as f:
+        reader = csv.reader(f, skipinitialspace=True)
+        reader.next()
+        # Split full comments into sentences
+        sentences = itertools.chain(*[nltk.sent_tokenize(x[0].decode("utf-8").lower()) for x in reader])
+        # Filter sentences
+        sentences = [s for s in sentences if len(s) >= min_sent_characters]
+        sentences = [s for s in sentences if "http" not in s]
+        # Append SENTENCE_START and SENTENCE_END
+        sentences = ["%s %s %s" % (SENTENCE_START_TOKEN, x, SENTENCE_END_TOKEN) for x in sentences]
+    print("Parsed %d sentences." % (len(sentences)))
+
+
+    # Tokenize the sentences into words
+    tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
+
+    # Count the word frequencies
+    word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
+    print("Found %d unique words tokens." % len(word_freq.items()))
+
+    # Get the most common words and build index_to_word and word_to_index vectors
+    vocab = sorted(word_freq.items(), key=lambda x: (x[1], x[0]), reverse=True)[:vocabulary_size-2]
+
+    print("Using vocabulary size %d." % vocabulary_size)
+    print("The least frequent word in our vocabulary is '%s' and appeared %d times." % (vocab[-1][0], vocab[-1][1]))
+
+    sorted_vocab = sorted(vocab, key=operator.itemgetter(1))
+
+    # concern about blackout algorithm
+    sorted_vocab.append(("<MASK/>",1))
+    sorted_vocab.append((UNKNOWN_TOKEN,1))
+
+    index_to_word = [x[0] for x in sorted_vocab]
+    word_to_index = dict([(w, i) for i, w in enumerate(index_to_word)])
+
+    # Replace all words not in our vocabulary with the unknown token
+    for i, sent in enumerate(tokenized_sentences):
+        tokenized_sentences[i] = [w if w in word_to_index else UNKNOWN_TOKEN for w in sent]
+
+    # Create the training data
+    X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in tokenized_sentences])
+    y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in tokenized_sentences])
+
+    if isfile('data/dataset.pkl'):
+        with open('data/dataset.pkl')as f:
+            (X_train, y_train, word_to_index, index_to_word, sorted_vocab)=pickle.load(f)
+            return X_train, y_train, word_to_index, index_to_word, sorted_vocab
+
+    return X_train, y_train, word_to_index, index_to_word, sorted_vocab
+
