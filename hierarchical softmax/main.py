226a53cde5163355735dc0cb34495f6593627e76
jiangnanHugo <jiangnan.hugo@gmail.com>
2016-10-02 21:47:27 +0800

    add 2 layer h_softmax

---
+++ hierarchical softmax/main.py
@@ -0,0 +1,62 @@
+import time
+
+from rnnlm import *
+from utils import TextIterator,save_model
+
+lr=0.01
+p=0.0
+n_batch=5
+NEPOCH=100
+
+n_input=250
+n_hidden=300
+maxlen=50
+cell='gru'
+optimizer='sgd'
+train_datafile='data/news.en-00001-of-00100'
+test_datafile='data/news.en.heldout-00000-of-00050'
+index2word_file='data/index2word.pkl'
+vocabulary_size=2000
+
+disp_freq=100
+sample_freq=10
+save_freq=300
+
+def train():
+    # Load data
+    print 'loading dataset...'
+    train_data=TextIterator(train_datafile,index2word_file,n_words_source=vocabulary_size,n_batch=n_batch,maxlen=maxlen)
+    test_data=TextIterator(test_datafile,index2word_file,n_words_source=vocabulary_size,n_batch=n_batch,maxlen=maxlen)
+
+    print 'building model...'
+    model=RNNLM(n_input,n_hidden,vocabulary_size,cell,optimizer,p)
+    print 'training start...'
+    start=time.time()
+    for epoch in xrange(NEPOCH):
+        error=0
+        idx=0
+        in_start=time.time()
+        for x,x_mask,y,y_mask in train_data:
+            idx+=1
+            cost=model.train(x,x_mask,y,y_mask,n_batch,lr)
+            print 'index:',idx,'cost:',cost
+            error+=np.sum(cost)
+            if np.isnan(cost) or np.isinf(cost):
+                print 'NaN Or Inf detected!'
+                return -1
+            if idx % disp_freq==0:
+                print 'epoch:',epoch,'idx:',idx,'cost:',error/disp_freq
+                error=0
+            if idx%save_freq==0:
+                print 'dumping...'
+                save_model('model/parameters_%.2f.pkl'%(time.time()-start),model)
+            if idx % sample_freq==0:
+                print 'Sampling....'
+                y_pred=model.predict(x,x_mask,n_batch)
+                print y_pred
+
+    print "Finished. Time = "+str(time.time()-start)
+
+
+if __name__ == '__main__':
+    train()
