76907f302005f725eeed11a19e26f69cdd4138ad
Nan Jiang <jiangnan.hugo@gmail.com>
2016-12-07 10:37:51 +0800

    def test func

--- gemm/gemm_validation.py
+++ gemm/gemm_validation.py
@@ -3,19 +3,23 @@ import numpy as np
 import theano
 import theano.tensor as T
 
-N=2
-A=theano.shared(np.random.randn(N*1024,1024).astype(theano.config.floatX),name="A")
-B=theano.shared(np.random.randn(N*1024,1024).astype(theano.config.floatX),name="B")
-C=T.dot(A,B)
+def matrix(N=2):
+    A=theano.shared(np.random.randn(1024,N*1024).astype(theano.config.floatX),name="A",borrow=True)
+    B=theano.shared(np.random.randn(N*1024,1024).astype(theano.config.floatX),name="B",borrow=True)
+    C=T.dot(A,B)
 
-model=theano.function([],C)
+    model=theano.function([],C)
 
-tmin=0
-for _ in range(10):
-	t0=time.time()
-	model()
-	t1=time.time()
-	tmin+=(t1-t0)
-	print t1-t0
+    tmin=0
+    for _ in range(20):
+	    t0=time.time()
+	    model()
+	    t1=time.time()
+	    tmin+=(t1-t0)
 
-print tmin*1.0/10
+    return tmin*1.0/10
+
+for index in range(2,100):
+    print index,matrix(index)
+
+
